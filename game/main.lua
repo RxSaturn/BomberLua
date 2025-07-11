4c3ddb383a4d5c7fe6209cf1f976f3e0682d8c62
Rx Saturn <0050718@academico.ifmg.edu.br>
2025-07-10 21:51:47 -0300

    Initial configurations

--- game/main.lua
+++ game/main.lua
@@ -1,9 +1,17 @@
-function love.load()
+if arg[2] == "debug" then
+    require("lldebugger").start()
 end
 
 function love.update()
 	print("Hello!")
 end
 
-function love.draw()
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
 end
